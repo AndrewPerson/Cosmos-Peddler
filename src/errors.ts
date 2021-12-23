ad6b06f9950aa9f2ba62f2db10af4957e69fc980
Andrew P <professor_smart@hotmail.com>
2021-12-23 16:32:44 +1100

    Updated errors

--- src/errors.ts
+++ src/errors.ts
@@ -1,13 +1,23 @@
 export class AuthenticationError extends Error {
     constructor() {
         super("Invalid SpaceTraders token");
+
+        this.name = "Authentication Error";
     }
 }
 
-export class ResourceError extends Error {}
+export class ResourceError extends Error {
+    constructor(message: string) {
+        super(message);
+
+        this.name = "Resource Error";
+    }
+}
 
 export class UninitialisedError extends Error {
     constructor() {
         super("Cosmos-Peddler Client is uninitialised");
+
+        this.name = "Uninitialised Error";
     }
 }
