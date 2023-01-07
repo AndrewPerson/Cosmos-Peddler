5cb3be04c034b86b94a05616ee63c7b84208d476
AndrewPerson <68579649+AndrewPerson@users.noreply.github.com>
2023-01-07 11:11:08 +1100

    Made star noise randomised

--- Game/Solar System/Stars/Orange Star/OrangeStarNode.cs
+++ Game/Solar System/Stars/Orange Star/OrangeStarNode.cs
@@ -7,6 +7,12 @@ public partial class OrangeStarNode : MeshInstance3D, IDimensionedObject
 	[Export]
 	public Vector3 Dimensions { get; private set; } = new Vector3(1, 1, 1);
 
+	public override void _Ready()
+	{
+		var seed = (float)GD.RandRange(0f, 10f);
+        SetSeed(seed);
+	}
+
 	public override void _Process(double delta)
 	{
 		var dist = Position.DistanceTo(GetViewport().GetCamera3d().Position);
@@ -14,5 +20,10 @@ public partial class OrangeStarNode : MeshInstance3D, IDimensionedObject
 		float brightness = 3 * Mathf.Log(Mathf.Max(dist / 2, 3));
 
 		SetInstanceShaderParameter("Glow_Power", brightness);
+	}
+
+	private void SetSeed(float seed)
+	{
+		SetInstanceShaderParameter("Seed", seed);
 	}
 }
