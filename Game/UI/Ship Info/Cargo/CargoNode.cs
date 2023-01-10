1e95dbe6272079021fdba70ed38590776b8b26ad
AndrewPerson <68579649+AndrewPerson@users.noreply.github.com>
2023-01-10 16:58:09 +1100

    Aligned naming conventions

--- Game/UI/Ship Info/Cargo/CargoNode.cs
+++ Game/UI/Ship Info/Cargo/CargoNode.cs
@@ -8,20 +8,20 @@ public partial class CargoNode : ReactiveUI<(ShipCargo, ShipNav)>
 	[Export]
 	public PackedScene cargoItemScene = null!;
 
-	private Label header = null!;
+	private Label name = null!;
 	private Node cargoItems = null!;
 
 	public override void _EnterTree()
 	{
-		header = GetNode<Label>("%Header");
+		name = GetNode<Label>("%Name");
 		cargoItems = GetNode<Node>("%Cargo List");
 	}
 
-    public override void UpdateUI()
+	public override void UpdateUI()
 	{
 		var (cargo, nav) = Data;
 
-		header.Text = $"Cargo - {cargo.Units}/{cargo.Capacity}";
+		name.Text = $"Cargo - {cargo.Units}/{cargo.Capacity}";
 
 		var items = cargo.Inventory.ToArray();
 
