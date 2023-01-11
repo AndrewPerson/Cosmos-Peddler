b161a0544795dc2f6f63520244a3dd94184ae9c3
AndrewPerson <68579649+AndrewPerson@users.noreply.github.com>
2023-01-11 15:59:45 +1100

    Added complete licenses

--- Home/AboutNode.cs
+++ Home/AboutNode.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System.Collections.Generic;
 
 namespace CosmosPeddler.Home;
 
@@ -8,6 +9,12 @@ public partial class AboutNode : Control
 {
 	private RichTextLabel license = null!;
 
+	private readonly Dictionary<string, string> licenses = new()
+	{
+		{ "Godot License", Engine.GetLicenseText() },
+		{ "Spaceship Model License", "Spaceship Concept by Yogoshimo 2.0 [CC-BY] (https://creativecommons.org/licenses/by/3.0/) via Poly Pizza (https://poly.pizza/m/9pBgIlbtj3F)" }
+	};
+
 	public override void _EnterTree()
 	{
 		license = GetNode<RichTextLabel>("%License");
@@ -15,16 +22,34 @@ public partial class AboutNode : Control
 
     public override void _Ready()
     {
-        license.PushParagraph(HorizontalAlignment.Center);
 		license.PushOutlineSize(3);
 		license.PushOutlineColor(Color.Color8(0, 0, 0));
 
-		license.AddText(Engine.GetLicenseText());
+		foreach (var (licenseName, licenseText) in licenses)
+		{
+			AddLicense(licenseName, licenseText);
+		}
+
+		foreach (var (licenseName, licenseText) in Engine.GetLicenseInfo())
+		{
+			AddLicense(licenseName.AsString(), licenseText.AsString());
+		}
 
 		license.Pop();
 		license.Pop();
-		license.Pop();
     }
+
+	private void AddLicense(string licenseName, string licenseText)
+	{
+		license.PushBold();
+		license.AddText($"{licenseName}:");
+		license.Newline();
+
+		license.AddText(licenseText);
+		license.Newline();
+		license.AddText(" ");
+		license.Newline();
+	}
 
 	public void OpenLink(string link)
 	{
